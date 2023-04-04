70d64e5895bde49aacdb581dc3da9763fc3332f5
hayrettins@sabanciuniv.edu <104569000+hayrettins@users.noreply.github.com>
2023-04-04 15:13:52 +0300

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href = "/">E-COMMERCE-CS308</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
